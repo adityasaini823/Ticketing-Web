94d4527a7b4aade69d302e38e928d281d047b6bf
adityasaini-primotech <aditya.saini@primotech.com>
2026-05-20 09:51:48 +0530

    added code for response format

--- auth/src/models/User.ts
+++ auth/src/models/User.ts
@@ -26,7 +26,16 @@ const userSchema = new mongoose.Schema({
         type: String,
         required: true
     }
-})
+},{
+        toJSON:{
+            transform(doc , ret){
+                ret.id = ret._id;
+                delete ret.password;
+                delete ret._id;
+                delete ret.__v;
+            }
+        }
+    })
 
 userSchema.pre("save", async function(){
     if(this.isModified("password")){
