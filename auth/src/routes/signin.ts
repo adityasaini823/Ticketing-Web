a0ca254f13b3b190e5031d8af13af6c1f134c7ab
aditya saini <adityasaini823@gmail.com>
2026-05-21 11:20:33 +0530

    added signin logic and validation middleware

--- auth/src/routes/signin.ts
+++ auth/src/routes/signin.ts
@@ -1,8 +1,32 @@
 import express from 'express';
 const router = express.Router();
+import { validateRequest } from "../middlewares/validate-request";
+import { User } from '../models/User';
+import { BadRequestError } from '../errors/bad-request-error';
+import { Password } from '../services/password';
+import jwt from 'jsonwebtoken';
+router.post("/signin", validateRequest, async (req, res) => {
+    const { email, password } = req.body;
+    // find excisting user
+    const excistingUser = User.findOne({ email });
+    if(!excistingUser){
+        throw new BadRequestError("Invalid credentials");
+    }
+    const passwordMatch =await Password.compare(excistingUser.password, password);
+     if(!passwordMatch){
+        throw new BadRequestError("Invalid credentials");
+     }
+    const userJwt = jwt.sign(
+        {
+        id: excistingUser._id,
+        email: excistingUser.email,
+        },
+        process.env.JWT_KEY!,
+    );
 
-router.post("/signin",(req,res)=>{
-    res.send("Welcome to signin route");
+    req.session = { jwt: userJwt };
+    return res.status(200).json({ msg: "User signed in successfully", user: excistingUser });
+    
 });
 
 export {router as signinRouter};
--- auth/src/routes/signup.ts
+++ auth/src/routes/signup.ts
@@ -1,39 +1,44 @@
-import express,{Request, Response} from 'express';
-import {body,validationResult} from 'express-validator';
-import { RequestValidationError } from '../errors/request-validation-error';
-import { BadRequestError } from '../errors/bad-request-error';
-import { User } from '../models/User';
-import jwt from 'jsonwebtoken';
+import express, { Request, Response } from "express";
+import { body, validationResult } from "express-validator";
+import { validateRequest } from "../middlewares/validate-request";
+import { BadRequestError } from "../errors/bad-request-error";
+import { User } from "../models/User";
+import jwt from "jsonwebtoken";
 const router = express.Router();
 
-router.post("/signup",[
+router.post(
+  "/signup",
+  [
     body("email").isEmail().withMessage("Email must be valid").normalizeEmail(),
-    body("password").trim().isLength({min: 6,max:20}).withMessage("Password must be at least 6 characters long")
-],async(req: Request, res: Response)=>{
-    const {email,password} = req.body;
-    const errors = validationResult(req);
-    if(!errors.isEmpty()){
-        throw new RequestValidationError(errors.array());
-    }
-     const existingUser = await User.findOne({email});
+    body("password")
+      .trim()
+      .isLength({ min: 6, max: 20 })
+      .withMessage("Password must be at least 6 characters long"),
+  ],
+  validateRequest,
+  async (req: Request, res: Response) => {
+    const { email, password } = req.body;
+    const existingUser = await User.findOne({ email });
     //  console.log("here");
-     if(existingUser){
-        console.log("User already exists with email: ", email);
-        throw new BadRequestError("User already exists");
-     }
-     const user = User.build({email,password});
-     // generate JWT
-     await user.save();
-     const userJwt = jwt.sign({
+    if (existingUser) {
+      console.log("User already exists with email: ", email);
+      throw new BadRequestError("User already exists");
+    }
+    const user = User.build({ email, password });
+    // generate JWT
+    await user.save();
+    const userJwt = jwt.sign(
+      {
         id: user._id,
-        email: user.email},
-        process.env.JWT_KEY!
+        email: user.email,
+      },
+      process.env.JWT_KEY!,
     );
 
-    // store jwt on session object  
-    req.session = {jwt: userJwt};
-    return res.status(200).json({msg: "User created successfully",user});
+    // store jwt on session object
+    req.session = { jwt: userJwt };
+    return res.status(200).json({ msg: "User created successfully", user });
+  },
+);
 
-});
-
-export {router as signupRouter};
+export { router as signupRouter };
