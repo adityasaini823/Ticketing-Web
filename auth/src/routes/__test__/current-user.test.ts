ea734561086b9e85315824ffa7dd1d50b483dfa3
adityasaini-primotech <aditya.saini@primotech.com>
2026-05-22 20:55:54 +0530

    added tests for current user session cokkie

---
+++ auth/src/routes/__test__/current-user.test.ts
@@ -0,0 +1,23 @@
+import request from "supertest"
+import { app } from "../../app";
+
+it("give the logged in user details", async () => {
+    const authresponse= await  request(app)
+    .post("/api/users/signup")
+    .send({
+        email: "test@test.com",
+        password: "password"
+    })
+    .expect(201);
+    const cookie = authresponse.get("Set-Cookie");
+    if (!cookie) {
+        console.log("Cookie not set after signup",authresponse.body);
+        throw new Error("Cookie not set after signup");
+    }
+    const response= await request(app)
+        .get("/api/users/currrentuser")
+        .set("Cookie", cookie)
+        .send({})
+        .expect(200);
+        console.log(response.body);
+});
--- auth/src/routes/__test__/signin.test.ts
+++ auth/src/routes/__test__/signin.test.ts
@@ -1,6 +1,5 @@
 import request from "supertest"
 import { app } from "../../app";
-import { get } from "node:http";
 
 it("fails when a email that does not exist is supplied", async () => {
     return request(app)
