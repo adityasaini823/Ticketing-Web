492699d8cf8e8d8401e2ce31466a787c9e23ba31
adityasaini-primotech <aditya.saini@primotech.com>
2026-05-22 17:28:53 +0530

    added tests for user session cokkie

--- auth/src/app.ts
+++ auth/src/app.ts
@@ -12,7 +12,7 @@ const app= express();
 app.set("trust proxy", true);
 app.use(cookieSession({
     signed: false,
-    secure: true
+    secure: process.env.NODE_ENV !== "test"
 }));
 app.use(express.json());
 app.use("/api/users", currentUserRouter);
--- auth/src/routes/__test__/signup.test.ts
+++ auth/src/routes/__test__/signup.test.ts
@@ -50,3 +50,13 @@ it("returns duplicate error", async () => {
         })
         .expect(400);
 });
+it("sets a cookie after successful signup", async () => {
+    const response = await request(app)
+        .post("/api/users/signup")
+        .send({
+            email: "test@example.com",
+            password: "password"
+        })
+        .expect(201);
+    expect(response.get("Set-Cookie")).toBeDefined();
+});
