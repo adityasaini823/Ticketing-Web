5275c583fbfe13b0441f9954e1c50ec5be5dd947
adityasaini-primotech <aditya.saini@primotech.com>
2026-06-12 19:13:33 +0530

    added code for new packgae changes

--- common/src/middlewares/require-auth.ts
+++ common/src/middlewares/require-auth.ts
@@ -1,7 +1,6 @@
 import { Request,Response,NextFunction } from "express";
 import {NotAuthorizedError} from "../errors/not-authorized-error";
-export const requireAuth = async(req: Request, res: Response, next: NextFunction) => {
-    console.log("Current user in requireAuth middleware:", req.currentUser);
+export const requireAuth = (req: Request, res: Response, next: NextFunction) => {
     if(!req.currentUser){
         throw new NotAuthorizedError();
     }
