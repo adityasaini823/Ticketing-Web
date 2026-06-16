d141204114845457d3489c0e8b2f273d2fd391f1
aditya saini <adityasaini823@gmail.com>
2026-06-16 10:36:26 +0530

    added code for chnages in commojn module

--- common/src/errors/not-found-error.ts
+++ common/src/errors/not-found-error.ts
@@ -1,12 +1,15 @@
-import { CustomError } from "./custom-error";
+import { CustomError } from './custom-error';
 
 export class NotFoundError extends CustomError {
-    statusCode = 404;
-    constructor(public message: string = "Route not found"){
-        super(message);
-        Object.setPrototypeOf(this,NotFoundError.prototype);
-    }
-    serializeErrors(){
-        return [{message: this.message}];
-    }
+  statusCode = 404;
+
+  constructor() {
+    super('Route not found');
+
+    Object.setPrototypeOf(this, NotFoundError.prototype);
+  }
+
+  serializeErrors() {
+    return [{ message: 'Not Found' }];
+  }
 }
