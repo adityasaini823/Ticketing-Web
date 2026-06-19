b954f8b8d94b2c1ca3794e1bc2a73a89c1b0ba57
adityasaini-primotech <aditya.saini@primotech.com>
2026-06-19 13:11:54 +0530

    added code new route and tests

--- tickets/src/routes/new.ts
+++ tickets/src/routes/new.ts
@@ -7,7 +7,7 @@ import express from "express";
 const router = express.Router();
 
 router.post(
-  '/api/tickets',
+  '/',
   requireAuth,
   [
     body('title').not().isEmpty().withMessage('Title is required'),
